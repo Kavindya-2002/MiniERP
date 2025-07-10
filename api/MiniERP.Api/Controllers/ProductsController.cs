0efe59cd01060500da852afb05e3d9798f937624
Kavindya Bandara <kavindya456789@gmail.com>
2025-07-10 10:03:53 +0530

    Added Product API and cleaned up .vs folder

---
+++ api/MiniERP.Api/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MiniERP.Data;
+using MiniERP.Models;
+
+namespace MiniERP.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProductsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> AddProduct(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetProducts), new { id = product.ProductId }, product);
+        }
+    }
+}
---
+++ api/MiniERP.Api/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using MiniERP.Models; // This assumes you’ll create models like Product.cs
+
+namespace MiniERP.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; } // We'll create Product.cs next
+    }
+}
